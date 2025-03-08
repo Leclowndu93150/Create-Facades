bd98e4e791619fc02e61682a93881812293e7eb4
Leclowndu93150 <xdr.a123454321@gmail.com>
2025-03-08 20:23:16 +0100

    1.20.1

--- src/main/java/com/leclowndu93150/create_facades/CreateFacades.java
+++ src/main/java/com/leclowndu93150/create_facades/CreateFacades.java
@@ -1,15 +1,13 @@
 package com.leclowndu93150.create_facades;
 
 import com.portingdeadmods.cable_facades.api.CableFacadesAPI;
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod;
 
 @Mod(CreateFacades.MODID)
 public class CreateFacades {
     public static final String MODID = "create_facades";
 
-    public CreateFacades(IEventBus modEventBus, ModContainer modContainer) {
+    public CreateFacades() {
         CableFacadesAPI.enqueueAPICallback(
                 api -> {
                     api.registerAllowedBlocks("create*");
